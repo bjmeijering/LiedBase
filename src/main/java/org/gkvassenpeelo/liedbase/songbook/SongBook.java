d0dc0ea506eedfc5be14878c50da2ef082375fc1
hdoedens <hdoedens@gmail.com>
2015-05-17 14:08:00 +0200

    added some unittests fixed next song header for opwekking songs added title page for boekje

--- src/main/java/org/gkvassenpeelo/liedbase/songbook/SongBook.java
+++ src/main/java/org/gkvassenpeelo/liedbase/songbook/SongBook.java
@@ -11,284 +11,286 @@ import org.gkvassenpeelo.liedbase.liturgy.SlideContents;
 
 public class SongBook {
 
-	static final Logger logger = Logger.getLogger(SongBook.class);
+    static final Logger logger = Logger.getLogger(SongBook.class);
 
-	/**
-	 * 
-	 * @param type
-	 * @param songNumber
-	 * @param verse
-	 * @return
-	 * @throws SongBookException
-	 */
-	public static List<SongLine> getSongText(SlideContents.Type type, String songNumber, String verse) throws SongBookException {
+    /**
+     * 
+     * @param type
+     * @param songNumber
+     * @param verse
+     * @return
+     * @throws SongBookException
+     */
+    public static List<SongLine> getSongText(SlideContents.Type type, String songNumber, String verse) throws SongBookException {
 
-		List<SongLine> songText = new ArrayList<SongLine>();
+        List<SongLine> songText = new ArrayList<SongLine>();
 
-		String songBookName = "";
-		String songIdentifier = "";
+        String songBookName = "";
+        String songIdentifier = "";
 
-		if (type == SlideContents.Type.psalm) {
-			songBookName = "psalmen.txt";
-			songIdentifier = "psalm";
-		}
+        if (type == SlideContents.Type.psalm) {
+            songBookName = "psalmen.txt";
+            songIdentifier = "psalm";
+        }
 
-		if (type == SlideContents.Type.gezang) {
-			songBookName = "gezangen.txt";
-			songIdentifier = "gereformeerd kerkboek";
-		}
+        if (type == SlideContents.Type.gezang) {
+            songBookName = "gezangen.txt";
+            songIdentifier = "gereformeerd kerkboek";
+        }
 
-		if (type == SlideContents.Type.lied) {
-			songBookName = "liedboek.txt";
-			songIdentifier = "lied";
-		}
+        if (type == SlideContents.Type.lied) {
+            songBookName = "liedboek.txt";
+            songIdentifier = "lied";
+        }
 
-		Scanner s = new Scanner(ClassLoader.getSystemResourceAsStream("songs/" + songBookName), LiturgyModel.ENCODING);
+        Scanner s = new Scanner(ClassLoader.getSystemResourceAsStream("songs/" + songBookName), LiturgyModel.ENCODING);
 
-		int songInteger = 0;
-		int verseInteger = 0;
-		String songNumberPostfix = "";
-		String nextSongNumberPostfix = "";
+        int songInteger = 0;
+        int verseInteger = 0;
+        String songNumberPostfix = "";
+        String nextSongNumberPostfix = "";
 
-		try {
-			verseInteger = Integer.parseInt(verse);
-		} catch (NumberFormatException e) {
-			s.close();
-			throw new SongBookException(String.format("'%s' is geen geldig nummer", verse));
-		}
+        try {
+            verseInteger = Integer.parseInt(verse);
+        } catch (NumberFormatException e) {
+            s.close();
+            throw new SongBookException(String.format("'%s' is geen geldig nummer", verse));
+        }
 
-		if (songNumber.matches("^[0-9]+$")) {
-			songInteger = Integer.parseInt(songNumber);
-		} else if (songNumber.matches("^[0-9]+[a-z]{1}$")) {
-			songInteger = Integer.parseInt(songNumber.substring(0, songNumber.length() - 1));
-			songNumberPostfix = songNumber.substring(songNumber.length() - 1, songNumber.length());
-			int charValue = songNumberPostfix.charAt(0);
-			nextSongNumberPostfix = String.valueOf((char) (charValue + 1));
-		}
+        if (songNumber.matches("^[0-9]+$")) {
+            songInteger = Integer.parseInt(songNumber);
+        } else if (songNumber.matches("^[0-9]+[a-z]{1}$")) {
+            songInteger = Integer.parseInt(songNumber.substring(0, songNumber.length() - 1));
+            songNumberPostfix = songNumber.substring(songNumber.length() - 1, songNumber.length());
+            int charValue = songNumberPostfix.charAt(0);
+            nextSongNumberPostfix = String.valueOf((char) (charValue + 1));
+        }
 
-		while (s.hasNextLine()) {
+        while (s.hasNextLine()) {
 
-			String line = s.nextLine();
+            String line = s.nextLine();
 
-			if (line.matches(String.format("^%s %s:.*$", songIdentifier, songNumber))) {
-				// we have the line number on which the song starts
-				// continue reading from that line again until we end up on
-				// the right verse
-				while (s.hasNextLine()) {
+            if (line.matches(String.format("^%s %s:.*$", songIdentifier, songNumber))) {
+                // we have the line number on which the song starts
+                // continue reading from that line again until we end up on
+                // the right verse
+                while (s.hasNextLine()) {
 
-					line = s.nextLine();
+                    line = s.nextLine();
 
-					// reading next song, return
-					if (readingNextSong(songIdentifier, songInteger, songNumberPostfix, nextSongNumberPostfix, line)) {
-						s.close();
-						return cleanup(songText);
-					}
+                    // reading next song, return
+                    if (readingNextSong(songIdentifier, songInteger, songNumberPostfix, nextSongNumberPostfix, line)) {
+                        s.close();
+                        return cleanup(songText);
+                    }
 
-					int index = 0;
-					if (line.equals(verse)) {
-						while (s.hasNextLine()) {
-							line = s.nextLine();
-							// reading next verse, return
-							// check for next verse AND next song!
-							if (line.equals("" + (verseInteger + 1)) || readingNextSong(songIdentifier, songInteger, songNumberPostfix, nextSongNumberPostfix, line)) {
-								s.close();
-								return cleanup(songText);
-							}
+                    int index = 0;
+                    if (line.equals(verse)) {
+                        while (s.hasNextLine()) {
+                            line = s.nextLine();
+                            // reading next verse, return
+                            // check for next verse AND next song!
+                            if (line.equals("" + (verseInteger + 1)) || readingNextSong(songIdentifier, songInteger, songNumberPostfix, nextSongNumberPostfix, line)) {
+                                s.close();
+                                return cleanup(songText);
+                            }
 
-							if (index == 0) {
-								songText.add(new SongLine(SongLine.DisplayType.first_line, line));
-							} else {
-								songText.add(new SongLine(SongLine.DisplayType.normal, line));
-							}
+                            if (index == 0) {
+                                songText.add(new SongLine(SongLine.DisplayType.first_line, line));
+                            } else {
+                                songText.add(new SongLine(SongLine.DisplayType.normal, line));
+                            }
 
-							index++;
-						}
-						s.close();
+                            index++;
+                        }
+                        s.close();
 
-						return cleanup(songText);
-					}
-				}
-			}
-		}
+                        return cleanup(songText);
+                    }
+                }
+            }
+        }
 
-		s.close();
+        s.close();
 
-		// no text found
-		return null;
-	}
+        // no text found
+        return null;
+    }
 
-	private static boolean readingNextSong(String songIdentifier, int songInteger, String songNumberPostfix, String nextSongNumberPostfix, String line) {
-		if (line.matches(String.format("^%s %s%s:.*$", songIdentifier, songInteger + 1, songNumberPostfix))) {
-			return true;
-		}
-		if (line.matches(String.format("^%s %s%s:.*$", songIdentifier, songInteger, nextSongNumberPostfix))) {
-			return true;
-		}
+    private static boolean readingNextSong(String songIdentifier, int songInteger, String songNumberPostfix, String nextSongNumberPostfix, String line) {
+        if (line.matches(String.format("^%s %s%s:.*$", songIdentifier, songInteger + 1, songNumberPostfix))) {
+            return true;
+        }
+        if (line.matches(String.format("^%s %s%s:.*$", songIdentifier, songInteger, nextSongNumberPostfix))) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	// private boolean readingNextSong(String line)
+    // private boolean readingNextSong(String line)
 
-	private static List<SongLine> cleanup(List<SongLine> songText) {
+    private static List<SongLine> cleanup(List<SongLine> songText) {
 
-		if (songText.size() > 0) {
+        if (songText.size() > 0) {
 
-			if (((SongLine) songText.get(songText.size() - 1)).getContent().equals("")) {
-				songText.remove(songText.size() - 1);
-			}
+            if (((SongLine) songText.get(songText.size() - 1)).getContent().equals("")) {
+                songText.remove(songText.size() - 1);
+            }
 
-			return songText;
+            return songText;
 
-		} else {
-			return null;
-		}
+        } else {
+            return null;
+        }
 
-	}
+    }
 
-	public static List<List<SongLine>> getOpwekkingSongTekst(String songNumber) throws SongBookException {
-		List<List<SongLine>> verses = new ArrayList<List<SongLine>>();
+    public static List<List<SongLine>> getOpwekkingSongTekst(String songNumber) throws SongBookException {
+        List<List<SongLine>> verses = new ArrayList<List<SongLine>>();
 
-		String songBookName = "opwekking.txt";
-		String songIdentifier = "opwekking";
+        String songBookName = "opwekking.txt";
+        String songIdentifier = "opwekking";
 
-		Scanner s = new Scanner(ClassLoader.getSystemResourceAsStream("songs/" + songBookName), LiturgyModel.ENCODING);
+        Scanner s = new Scanner(ClassLoader.getSystemResourceAsStream("songs/" + songBookName), LiturgyModel.ENCODING);
 
-		while (s.hasNextLine()) {
+        while (s.hasNextLine()) {
 
-			String line = s.nextLine();
+            String line = s.nextLine();
 
-			if (line.matches(String.format("^%s %s$", songIdentifier, songNumber))) {
-				// we have the line number on which the song starts
-				// continue reading from that line again until we have all song
-				// parts
+            if (line.matches(String.format("^%s %s$", songIdentifier, songNumber))) {
+                // we have the line number on which the song starts
+                // continue reading from that line again until we have all song
+                // parts
 
-				// do two extra readlines (into oblivion) so the songtitle and
-				// the blank line thereafter are skipped
-				s.nextLine();
-				s.nextLine();
+                // do two extra readlines (into oblivion) so the songtitle and
+                // the blank line thereafter are skipped
+                s.nextLine();
+                s.nextLine();
 
-				while (s.hasNextLine()) {
+                while (s.hasNextLine()) {
 
-					// if (StringUtils.isEmpty(songLine) ||
-					// StringUtils.startsWith(songLine, "(c)") ||
-					// "Ned. tekst  arr.: Opwekking".equals(songLine)) {
-					// continue;
-					// }
+                    // if (StringUtils.isEmpty(songLine) ||
+                    // StringUtils.startsWith(songLine, "(c)") ||
+                    // "Ned. tekst  arr.: Opwekking".equals(songLine)) {
+                    // continue;
+                    // }
 
-					boolean nextSong = false;
-					List<SongLine> verseText = new ArrayList<SongLine>();
+                    boolean nextSong = false;
+                    List<SongLine> verseText = new ArrayList<SongLine>();
 
-					while (s.hasNextLine() && !nextSong) {
+                    while (s.hasNextLine() && !nextSong) {
 
-						// read next line
-						String verseLine = s.nextLine();
+                        // read next line
+                        String verseLine = s.nextLine();
 
-						// check to see if we are not reading too far
-						nextSong = verseLine.matches(String.format("^%s %s$", songIdentifier, Integer.parseInt(songNumber) + 1));
+                        // check to see if we are not reading too far
+                        nextSong = verseLine.matches(String.format("^%s %s$", songIdentifier, Integer.parseInt(songNumber) + 1));
 
-						if (StringUtils.isEmpty(verseLine)) {
-							if (!StringUtils.isEmpty(verseText.toString()) && !StringUtils.startsWith(verseText.toString(), "Tekst  muziek:")
-									&& !StringUtils.startsWith(verseText.toString(), "(c)")) {
-								verses.add(verseText);
-							}
-							verseText = new ArrayList<SongLine>();
-							continue;
-						}
-						verseText.add(new SongLine(SongLine.DisplayType.normal, verseLine));
-					}
-					
-					// add the last verse
-					verses.add(verseText);
+                        if (StringUtils.isEmpty(verseLine)) {
+                            if (!StringUtils.isEmpty(verseText.toString()) && !StringUtils.startsWith(verseText.toString(), "Tekst  muziek:")
+                                    && !StringUtils.startsWith(verseText.toString(), "(c)")) {
+                                verses.add(verseText);
+                            }
+                            verseText = new ArrayList<SongLine>();
+                            continue;
+                        }
+                        verseText.add(new SongLine(SongLine.DisplayType.normal, verseLine));
+                    }
 
-					return verses;
-				}
+                    // add the last verse/line, unless the line is the header of the next song
+                    if (!nextSong) {
+                        verses.add(verseText);
+                    }
 
-				s.close();
-				return verses;
-			}
-		}
+                    return verses;
+                }
 
-		// song not found
-		s.close();
-		throw new SongBookException("Opwekking " + songNumber + " niet gevonden");
-	}
+                s.close();
+                return verses;
+            }
+        }
 
-	public static List<String> getVersesFromSong(SlideContents.Type type, String songNumber) {
-		List<String> verses = new ArrayList<String>();
+        // song not found
+        s.close();
+        throw new SongBookException("Opwekking " + songNumber + " niet gevonden");
+    }
 
-		String songBookName = "";
-		String songIdentifier = "";
+    public static List<String> getVersesFromSong(SlideContents.Type type, String songNumber) {
+        List<String> verses = new ArrayList<String>();
 
-		if (type == SlideContents.Type.psalm) {
-			songBookName = "psalmen.txt";
-			songIdentifier = "psalm";
-		}
+        String songBookName = "";
+        String songIdentifier = "";
 
-		if (type == SlideContents.Type.gezang) {
-			songBookName = "gezangen.txt";
-			songIdentifier = "gereformeerd kerkboek";
-		}
+        if (type == SlideContents.Type.psalm) {
+            songBookName = "psalmen.txt";
+            songIdentifier = "psalm";
+        }
 
-		if (type == SlideContents.Type.lied) {
-			songBookName = "liedboek.txt";
-			songIdentifier = "lied";
-		}
+        if (type == SlideContents.Type.gezang) {
+            songBookName = "gezangen.txt";
+            songIdentifier = "gereformeerd kerkboek";
+        }
 
-		Scanner s = new Scanner(ClassLoader.getSystemResourceAsStream("songs/" + songBookName), LiturgyModel.ENCODING);
+        if (type == SlideContents.Type.lied) {
+            songBookName = "liedboek.txt";
+            songIdentifier = "lied";
+        }
 
-		while (s.hasNextLine()) {
+        Scanner s = new Scanner(ClassLoader.getSystemResourceAsStream("songs/" + songBookName), LiturgyModel.ENCODING);
 
-			String line = s.nextLine();
+        while (s.hasNextLine()) {
 
-			if (line.matches(String.format("^%s %s:.*$", songIdentifier, songNumber))) {
-				// we have the line number on which the song starts
-				// continue reading from that line until we have all verses
-				while (s.hasNextLine()) {
-					String songLine = s.nextLine();
+            String line = s.nextLine();
 
-					// we are reading the next song, stop!
-					String tempSongNumber = songNumber;
-					if (tempSongNumber.matches("^[0-9]*[a-z]{1}$")) {
-						tempSongNumber = songNumber.substring(0, songNumber.length() - 1);
-					}
-					// next song in integers
-					if (songLine.matches(String.format("^%s %s[a-z]*:.*$", songIdentifier, Integer.parseInt(tempSongNumber) + 1))) {
-						s.close();
-						return verses;
-					}
-					// next song alphabetically
-					if (songLine.matches(String.format("^%s %s[a-z]*:.*$", songIdentifier, Integer.parseInt(tempSongNumber)))) {
-						s.close();
-						return verses;
-					}
+            if (line.matches(String.format("^%s %s:.*$", songIdentifier, songNumber))) {
+                // we have the line number on which the song starts
+                // continue reading from that line until we have all verses
+                while (s.hasNextLine()) {
+                    String songLine = s.nextLine();
 
-					try {
-						Integer.parseInt(songLine);
-						verses.add(songLine);
-					} catch (Exception e) {
-						// do nothing
-					}
-				}
-			}
-		}
+                    // we are reading the next song, stop!
+                    String tempSongNumber = songNumber;
+                    if (tempSongNumber.matches("^[0-9]*[a-z]{1}$")) {
+                        tempSongNumber = songNumber.substring(0, songNumber.length() - 1);
+                    }
+                    // next song in integers
+                    if (songLine.matches(String.format("^%s %s[a-z]*:.*$", songIdentifier, Integer.parseInt(tempSongNumber) + 1))) {
+                        s.close();
+                        return verses;
+                    }
+                    // next song alphabetically
+                    if (songLine.matches(String.format("^%s %s[a-z]*:.*$", songIdentifier, Integer.parseInt(tempSongNumber)))) {
+                        s.close();
+                        return verses;
+                    }
 
-		s.close();
+                    try {
+                        Integer.parseInt(songLine);
+                        verses.add(songLine);
+                    } catch (Exception e) {
+                        // do nothing
+                    }
+                }
+            }
+        }
 
-		return verses;
-	}
+        s.close();
 
-	/**
-	 * 
-	 * @param line
-	 * @return
-	 */
-	public static String getSongNumber(String line) {
-		if (line.contains(":")) {
-			return StringUtils.substringBetween(line, " ", ":").trim();
-		} else {
-			return StringUtils.substringAfter(line, " ");
-		}
-	}
+        return verses;
+    }
+
+    /**
+     * 
+     * @param line
+     * @return
+     */
+    public static String getSongNumber(String line) {
+        if (line.contains(":")) {
+            return StringUtils.substringBetween(line, " ", ":").trim();
+        } else {
+            return StringUtils.substringAfter(line, " ");
+        }
+    }
 
 }
--- src/test/java/org/gkvassenpeelo/liedbase/songbook/SongBookTest.java
+++ src/test/java/org/gkvassenpeelo/liedbase/songbook/SongBookTest.java
@@ -1,6 +1,57 @@
 package org.gkvassenpeelo.liedbase.songbook;
 
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.gkvassenpeelo.liedbase.liturgy.SlideContents;
+import org.junit.Test;
+
 public class SongBookTest {
 
-    // TODO: Test: Gezang 111
+    @Test
+    public void getOpwekking194Text() {
+        try {
+            List<List<SongLine>> songText = SongBook.getOpwekkingSongTekst("194");
+
+            // assert first line
+            assertEquals("U maakt ons één.", songText.get(0).get(0).getContent());
+            // assert last line
+            assertEquals("Ned. tekst: JmeO", songText.get(songText.size() - 1).get(songText.get(songText.size() - 1).size() - 1).getContent());
+
+        } catch (SongBookException e) {
+            fail("test failed with error: " + e.getMessage());
+        }
+    }
+    
+    @Test
+    public void getOpwekking717Text() {
+        try {
+            List<List<SongLine>> songText = SongBook.getOpwekkingSongTekst("717");
+            
+            // assert first line
+            assertEquals("Stil mijn ziel wees stil,", songText.get(0).get(0).getContent());
+            // assert last line
+            assertEquals("Ik rust in U alleen.", songText.get(songText.size() - 1).get(songText.get(songText.size() - 1).size() - 1).getContent());
+            
+        } catch (SongBookException e) {
+            fail("test failed with error: " + e.getMessage());
+        }
+    }
+    
+    @Test
+    public void getGezang111Text() {
+        try {
+            List<SongLine> songText = SongBook.getSongText(SlideContents.Type.gezang, "111", "1");
+            
+            // assert first line
+            assertEquals("refrein", songText.get(0).getContent());
+            // assert last line
+            assertEquals("Hij is de Heer van mijn leven.", songText.get(songText.size() - 1).getContent());
+            
+        } catch (SongBookException e) {
+            fail("test failed with error: " + e.getMessage());
+        }
+    }
 }
