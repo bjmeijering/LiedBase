fdd00ba9572b20bf9504a69d9ea25ee79654a271
hdoedens <hdoedens@gmail.com>
2015-08-17 12:19:29 +0200

    changed logging somewhat

--- src/main/java/org/gkvassenpeelo/liedbase/LiedBase.java
+++ src/main/java/org/gkvassenpeelo/liedbase/LiedBase.java
@@ -2,6 +2,7 @@ package org.gkvassenpeelo.liedbase;
 
 import javax.swing.UnsupportedLookAndFeelException;
 
+//import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.gkvassenpeelo.liedbase.GUI.LiedBaseView;
 import org.gkvassenpeelo.liedbase.liturgy.LiturgyModel;
@@ -18,19 +19,26 @@ import org.gkvassenpeelo.liedbase.slidemachine.SlideMachineException;
  */
 public class LiedBase {
 
-	static final Logger logger = Logger.getLogger(LiedBase.class);
+    static final Logger logger = Logger.getLogger(LiedBase.class);
 
-	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException, SlideMachineException,
-			PaperMachineException {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException, SlideMachineException,
+            PaperMachineException {
 
-		LiturgyModel model = new LiturgyModel(logger);
-		LiedBaseController controller = new LiedBaseController(model);
-		LiedBaseView view = new LiedBaseView(controller);
+//        // Configure logger
+//        BasicConfigurator.configure();
+//        logger.debug("Hello World!");
+//        logger.info("Info");
+//        logger.warn("warning!");
+//        logger.error("error");
 
-		view.initialize();
-		view.setVisible(true);
+        LiturgyModel model = new LiturgyModel(logger);
+        LiedBaseController controller = new LiedBaseController(model);
+        LiedBaseView view = new LiedBaseView(controller);
 
-		logger.info("LiedBase gestart");
+        view.initialize();
+        view.setVisible(true);
 
-	}
+        logger.info("LiedBase gestart");
+
+    }
 }
